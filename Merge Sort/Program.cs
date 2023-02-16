03a1ce7393999f934c7ae16fbaa9900385832a6b
ToCatchTo <119807755+ToCatchTo@users.noreply.github.com>
2023-02-16 12:34:30 +0100

    Add files via upload

---
+++ Merge Sort/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Merge_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte délku řady:");
+            string length = Console.ReadLine();
+
+            //Vytvoří list a rnd generator
+            List<int> rada = new List<int>();
+            List<int> tmpRada = new List<int>();
+            Random rnd = new Random();
+
+            //Naplní list
+            for (int i = 0; i < Convert.ToInt32(length); i++)
+            {
+                int rndNumber = rnd.Next(-20, 21);
+                rada.Add(rndNumber);
+            }
+
+            for (int i = 0; i < Convert.ToInt32(length); i++)
+            {
+                tmpRada.Add(0);
+            }
+
+            //Konvertuje list na pole
+            int[] a = rada.ToArray();
+
+            //Vypíše nesetřízené pole
+            for (int h = 0; h < a.Length; h++)
+            {
+                Console.Write(a[h] + " ");
+            }
+            Console.ReadLine();
+
+            MergeSort(a);
+        }
+
+        public static void Merge(int[] list, int[] left, int[] right)
+        {
+            int i = 0; // index levého pole
+            int j = 0; // index pravého pole
+
+            while ((i < left.Length) && (j < right.Length)) // procházíme do té doby, než dojdeme na konec jednoho z polí
+            {
+                if (left[i] < right[j]) // pokud je levý prvek menší, dá se do výsledného pole levý prvek
+                {
+                    list[i + j] = left[i];
+                    i++;
+                }
+                else // pokud je pravý prvek menší, dá se do výsledného pole pravý prvek
+                {
+                    list[i + j] = right[j];
+                    j++;
+                }
+            }
+
+            if (i < left.Length) // jestli jedna půlka byla větší než ta druhá tak zbyde poslední prvek
+            {
+                while (i < left.Length)
+                {
+                    list[i + j] = left[i];
+                    i++;
+                }
+            }
+            else
+            {
+                while (j < right.Length)
+                {
+                    list[i + j] = right[j];
+                    j++;
+                }
+            }
+
+            for (int h = 0; h < list.Length; h++)
+            {
+                if (h == 0)
+                    Console.WriteLine("Proběhlo spojení:");
+                Console.Write(list[h] + " ");
+            }
+            Console.ReadLine();
+        }
+
+        public static void MergeSort(int[] list)
+        {
+            for (int h = 0; h < list.Length; h++)
+            {
+                Console.Write(list[h] + " ");
+            }
+            Console.ReadLine();
+            if (list.Length <= 1) //pole musí být delší než 1, aby dalo třídit
+                return;
+            int center = list.Length / 2; //určíme si střed pole, díky kterému mužeme pole rozdělit
+            int[] left = new int[center]; //levá část se vytvoří a naplní
+            for (int i = 0; i < center; i++)
+                left[i] = list[i];
+            int[] right = new int[list.Length - center]; //pravá část se vytvoří a naplní
+            for (int i = center; i < list.Length; i++)
+                right[i - center] = list[i];
+            MergeSort(left); // pomocí rekurze tak budeme rozdělovat vzniklá pole dál a dál až do samotných prvků
+            MergeSort(right);
+            Merge(list, left, right); //nakonec se pole postupně spojí a setřídí
+        }
+    }
+}
---
+++ Quick sort/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quick_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dostane délku od uživatele
+            Console.WriteLine("Zadejte délku řady:");
+            string length = Console.ReadLine();
+
+            //Vytvoří list a rnd generator
+            List<int> rada = new List<int>();
+            Random rnd = new Random();
+
+            //Naplní list
+            for (int i = 0; i < Convert.ToInt32(length); i++)
+            {
+                int rndNumber = rnd.Next(-20, 21);
+                rada.Add(rndNumber);
+            }
+
+            //Konvertuje list na pole
+            int[] a = rada.ToArray();
+
+            //Vypíše nesetřízené pole
+            for (int h = 0; h < a.Length; h++)
+            {
+                Console.Write(a[h] + " ");
+            }
+            Console.ReadLine();
+
+            //Zavolám sortící metodu
+            Quicksort(a, 0, a.Length);
+        }
+
+        public static void Quicksort(int[] array, int left, int right) //předávám pole, index prvního, index posledního
+        {
+            if (left < right) // 
+            {
+                int boundary = left; // 
+                for (int i = left + 1; i < right; i++)
+                {
+                    if (array[i] > array[left])
+                    {
+                        Swap(array, i, ++boundary);
+                    }
+                }
+                Swap(array, left, boundary);
+
+                for (int h = 0; h < array.Length; h++)
+                {
+                    Console.Write(array[h] + " ");
+                }
+                Console.ReadLine();
+
+                Quicksort(array, left, boundary);
+                Quicksort(array, boundary + 1, right);
+            }
+        }
+
+        /**
+         * Prohodi prvky v zadanem poli
+         * @param array pole
+         * @param left prvek 1
+         * @param right prvek 2
+         */
+        private static void Swap(int[] array, int left, int right)
+        {
+            int tmp = array[right];
+            array[right] = array[left];
+            array[left] = tmp;
+        }
+    }
+}
